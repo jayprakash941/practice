bbdd09d87717f012ade4ef1e4a7996160f37706d
jayprakash941 <yadav.jayprakash19@gmail.com>
2022-09-15 00:00:56 +0530

    1 file modified

--- google_tech_guide/4Sum.js
+++ google_tech_guide/4Sum.js
@@ -56,3 +56,48 @@ const target = 0;
 // console.log(fourSum(nums, target));
 
 console.log(fourSum(nums, target, 0));
+
+let fourSum = (nums, target) => {
+  let n = nums.length;
+  let res = [];
+  if (n < 4) {
+    return res;
+  }
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i <= n - 4; i++) {
+    let val = nums[i];
+    if (i == 0 || nums[i] !== nums[i - 1]) {
+      threeSum(nums, n, target - val, i + 1).forEach((r) => {
+        res.push(r.concat(nums[i]));
+      });
+    }
+  }
+  return res;
+};
+
+let threeSum = (nums, n, target, index) => {
+  let res = [];
+  if (n - index < 3) return res;
+  for (let i = index; i < n - 2; i++) {
+    if (i == index || nums[i] !== nums[i - 1]) {
+      let p1 = i + 1;
+      let p2 = n - 1;
+      let sum = -nums[i] + target;
+      while (p1 < p2) {
+        if (nums[p1] + nums[p2] === sum) {
+          res.push([nums[i], nums[p1], nums[p2]]);
+          while (p1 < p2 && nums[p1] === nums[p1 + 1]) p1++;
+          while (p1 < p2 && nums[p2] === nums[p2 + 1]) p2--;
+          p1++;
+          p2--;
+        } else if (nums[p1] + nums[p2] < sum) p1++;
+        else p2--;
+      }
+    }
+  }
+  return res;
+};
+
+const nums = [0, 0, 0, 0];
+const target = 0;
+console.log(fourSum(nums, target));
